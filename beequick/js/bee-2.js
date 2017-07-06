56661a77ed1a84bbb03da536b88b2210eee28ff3
Huibinchen <1262918339@qq.com>
2017-07-06 10:44:14 +0800

    revise beequick

--- beequick/js/bee-2.js
+++ beequick/js/bee-2.js
@@ -43,7 +43,7 @@ angular.module("myApp",["ngRoute"]).config(["$routeProvider",function ($routePro
             controller : 'HomeCtrl'
         })
         .when('/menu',{
-            templateUrl:'./views/menu.html',
+            templateUrl:'./views/menu-2.html',
             controller : 'MenuCtrl'
         })
         .when('/car',{
@@ -66,7 +66,7 @@ angular.module("myApp",["ngRoute"]).config(["$routeProvider",function ($routePro
         $scope.slide = [];
         $scope.menu = [];
         $scope.product = [];
-        $http.get("http://h5.yztctech.net/api/axf/apihome.php").success(function (data){
+        $http.get("js/getApiHome.json").success(function (data){
             $scope.slide = data.data.slide.filter((item,index)=>index!=0&&index!=data.data.slide.length-1);
             $scope.menu = data.data.menu
             var imgNum = 0
@@ -89,56 +89,120 @@ angular.module("myApp",["ngRoute"]).config(["$routeProvider",function ($routePro
                 }
             }
         })
-        $http.get("http://h5.yztctech.net/api/axf/apimiaosha.php").success(function (data){
+        $http.get("js/getApiMiao.json").success(function (data){
             $scope.product = data.product.filter((item,index)=>index<=2);
         })
             // 获取热卖数据
         $scope.homehot = []
-        $http.get("http://h5.yztctech.net/api/axf/apihomehot.php").success(function (data){
+        $http.get("js/getApiHomeHot.json").success(function (data){
                 $scope.homehot = data.data.slice(0,3)
         })
         $scope.yin = []
-        $http.get("http://h5.yztctech.net/api/axf/apihomehot.php").success(function (data){
+        $http.get("js/getApiHomeHot.json").success(function (data){
             $scope.yin = data.data.slice(4,7)
         })
         $scope.yes = []
-        $http.get("http://h5.yztctech.net/api/axf/apihomehot.php").success(function (data){
+        $http.get("js/getApiHomeHot.json").success(function (data){
             $scope.yes = data.data.slice(8,11)
         })
     }])
+
+
+
     .controller("MenuCtrl",['$scope','$http',function ($scope,$http) {
+        // 用于存储所有分类数据
+        $scope.categories = []
+
+        // 存储所有的商品信息
+        $scope.products = {}
+
+        // 过滤的选项,可选为综合排序和分类排序
+        $scope.filterItems = []
+
+        // 综合排序,因为全部分类项返回的数据格式是对象，键为name，为了跟它统一
+        $scope.rankings = [{name:'综合排序'},{name:"价格最低"},{name:"价格最高"}]
+
+        // 全部分类
         $scope.allCategories = false
+        // 综合排序
         $scope.ranking = false
 
+        // 激活的分类id-> 初始为第一个
+        $scope.activeCategoriesId = 0
+        // 分类的商品数组
+        $scope.categoriesItems = []
+
+        $scope.key = "全部分类"
+        $scope.orderObj ={
+            // order排序的关键字
+            order:'',
+            // 升序还是降序
+            orderBol: false
+        }
+
+
+        //点击全部分类
         $scope.changeCategorieBol = function (){
             $scope.ranking = false
             $scope.allCategories = !$scope.allCategories
+            // 通过分类的id获取对应的cids也就是分类列表
+            for (var i=0; i<$scope.categories.length; i++){
+                if ($scope.categories[i].id == $scope.activeCategoriesId){
+                    $scope.filterItems = $scope.categories[i].cids
+                }
+            }
         }
+        //点击综合排序
         $scope.changeRankingBol = function (){
             $scope.allCategories = false
             $scope.ranking = !$scope.ranking
+            $scope.filterItems = $scope.rankings
         }
 
-        $scope.btnList = ["天天特价","优选水果","热销榜","牛奶面包"]
-        // 激活的分类下标
-        $scope.activeCategoriesIndex = 0
-        $scope.categoriesItems = [];
-        function getData() {
-            var url = "http://h5.yztctech.net/api/axf/apicategory.php?category="+$scope.btnList[$scope.activeCategoriesIndex]
+        $scope.filterProduct = function (item) {
+            if(allCategories){
 
-            $http.get(url).success(function (data){
-                var data = data.data
-                $scope.categoriesItems = data
+            }
+        }
+        // 初始获取热销榜的数据
+        function getData(){
+            $http.get("js/getCategoryProduct.json").success(function (data){
+
+                // 分类数据
+                $scope.categories = data.data.categories
+
+                // 初始化分类id
+                $scope.activeCategoriesId = data.data.categories[0].id
+
+                // 商品数据
+                $scope.products = data.data.products
             })
         }
         getData()
-        $scope.changeCategoriesIndex = function (index){
-            $scope.activeCategoriesIndex = index
 
-            getData()
+
+
+        // 点击分类切换
+        $scope.changeCategoriesIndex = function (id){
+            $scope.activeCategoriesId = id
+
+            // 将全部分类和综合排序隐藏掉
+            $scope.allCategories = false
+            $scope.ranking = false
+
+            // 分类关键字->默认为“全部分类”
+            $scope.key = "全部分类"
+            $scope.orderObj = {
+                // order排序的关键字
+                order:'',
+                // 升序还是降序
+                orderBol: false
+            }
         }
-
     }])
+
+
+
     // 当angular初始化完成会运行run
     .run(["$rootScope",function ($rootScope){
         $rootScope.navActiveIndex = 0
